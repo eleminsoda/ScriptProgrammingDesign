e09fd3a9494d9ecfc5a8b5c7cde25fc22340a10c
lord-maul <987420788yh@gmail.com>
2019-04-14 20:37:43 +0800

    finished homework3git add . hurraygit add .

--- homework_3/speedreader.js
+++ homework_3/speedreader.js
@@ -1,17 +1,41 @@
-window.onload = function () {
+'use strict'
+
+window.onload = function() {
     console.log("window.onload, start binding...");
+    // set click triggers
     document.getElementById('start').onclick = startAnimation;
     document.getElementById('stop').onclick = stopAnimation;
+
+    // set trigger for changes of the font size
+    let sizeNodes = document.getElementsByName("size");
+    for (let node of sizeNodes) {
+        node.onchange = changeFontSize;
+    }
+
+    // set trigger for changes of the display speed
+    document.getElementById('speed').onchange = changeDisplaySpeed;
+    disableButton('stop');
 }
+
+let displayList = []
+let interval = 0;
+let index = -1;
 
 function startAnimation() {
     let showArea = document.getElementById("show-area");
 
     let text = document.getElementById('text-input').value;
-    let wordList = text.split(' ');
+    let wordList = text.trim().split(' ');
 
+    if (wordList.length == 1 && wordList[0] === '') {
+        console.log("the text area is empty");
+        return;
+    } else {
+        setDisableStatus('start');
+    }
+
+    displayList = [];
     let punctuation = [',', '.', '!', '?', ':', ";", "，", "。", "？", "；", "："];
-    let displayList = [];
     for (let word of wordList) {
         let lastCh = word.charAt(word.length - 1);
         let newWord = '';
@@ -23,30 +47,116 @@ function startAnimation() {
         } else {
             displayList.push(word);
         }
-        console.log("word: " + word + ", newword: " + newWord);
     }
 
-    setTimeout(() => {
+    // because of the feature of clearInterval, we have to add the last word twice into the queue
+    let lastWord = displayList[displayList.length - 1];
+    displayList.push(lastWord);
 
-    }, 2000);
-    let index = 0;
-    let interval = setInterval(() => {
-        if (index === displayList.length - 1) {
-            clearInterval(interval);
-        }
-
+    let speed = parseInt(document.getElementById('speed').value);
+    index = 0;
+    // start displaying the words
+    interval = setInterval(() => {
         showArea.innerText = displayList[index];
         console.log("now index: " + index + ", word: " + displayList[index])
         index += 1;
-    }, 171);
-    // showArea.innerText = wordList.join('-');
+
+        if (index >= displayList.length) {
+            stopAnimation();
+        }
+    }, speed);
 }
 
 function stopAnimation() {
+    console.log("stop animation");
+    clearInterval(interval);
     let showArea = document.getElementById("show-area");
     showArea.innerText = "";
+    setDisableStatus('stop');
 }
 
-function changeTextDisplay() {
+function changeFontSize() {
+    let sizeNodes = document.getElementsByName("size");
+    for (let node of sizeNodes) {
+        // get the node that is checked
+        if (node.checked === true) {
+            console.log("Already found the fontSize node...");
 
+            let showArea = document.getElementById("show-area");
+            console.log("fontSize changed to " + node.value);
+            showArea.style.fontSize = node.value;
+            break;
+        }
+    }
 }
+
+function changeDisplaySpeed() {
+    let newSpeed = parseInt(document.getElementById('speed').value);
+    console.log('changing display speed to ' + newSpeed);
+
+    // clear the interval first
+    clearInterval(interval);
+
+    // set new interval based on new display speed
+    let showArea = document.getElementById("show-area");
+    let wordNow = showArea.innerText;
+
+    // if the display hasn't start yet...
+    if (index < 0) {
+        return;
+    }
+
+    interval = setInterval(() => {
+        showArea.innerText = displayList[index];
+        console.log("now index: " + index + ", word: " + displayList[index])
+        index += 1;
+
+        if (index >= displayList.length) {
+            stopAnimation();
+        }
+    }, newSpeed);
+}
+
+// disable the start or stop button
+function disableButton(type) {
+    let another = '';
+    if (type === 'start') {
+        another = 'stop';
+    } else if (type === 'stop') {
+        another = 'start';
+    } else {
+        alert("Type error!");
+        return;
+    }
+
+    let button = document.getElementById(type);
+    button.disabled = true;
+    button.style.backgroundColor = 'lightgray';
+
+    button = document.getElementById(another);
+    button.disabled = false;
+    button.style.backgroundColor = 'white';
+}
+
+function setTextArea() {
+    let textarea = document.getElementById('text-input');
+    let status = textarea.disabled;
+
+    if (status) {
+        textarea.disabled = false;
+    } else {
+        textarea.disabled = true;
+    }
+}
+
+function setDisableStatus(type) {
+    disableButton(type);
+    setTextArea();
+}
+
+// clear and reset the interval, and disable the button as well
+// function clearDisplay() {
+//     clearInterval(interval);
+//     interval = -1;
+//     setDisableStatus('stop');
+// }
