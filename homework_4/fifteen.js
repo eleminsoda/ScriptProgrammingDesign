471a5c8074b368e19e3bc338a96b34e0aadf4c04
lord-maul <987420788yh@gmail.com>
2019-04-21 00:16:20 +0800

    testing img

--- homework_4/fifteen.js
+++ homework_4/fifteen.js
@@ -9,6 +9,7 @@ window.onload = function () {
 
 let puzzleSize = 4;
 let puzzleNodes = [];
+let blankIndex = 16;
 
 function insertPuzzle() {
     let puzzleArea = document.getElementById('puzzlearea');
@@ -17,18 +18,23 @@ function insertPuzzle() {
     // create each node in for loop
     for (let i = 1; i <= puzzleCount; i++) {
         let newNode = document.createElement('div');
-        let row = i % puzzleSize;
-        let col = Math.ceil(i / puzzleSize);
+        let row = Math.floor((i - 1) / puzzleSize);
+        let col = (i - 1) % puzzleSize;
         newNode.id = "node_" + row + "_" + col;
 
         newNode.style.border = "5px solid black";
         newNode.style.width = "100px";
         newNode.style.height = "100px";
         newNode.style.position = "absolute";
-        newNode.style.left = (col - 1) * 100 + "px";
+        newNode.style.left = col * 100 + "px";
         newNode.style.top = row * 100 + "px";
+        newNode.style.backgroundImage = "url(background.jpg)";
 
-        newNode.innerText = row + "_" + col;
+
+        newNode.style.clip = "rect(" + row * 100 + "px," + (col + 1) * 100 + "px," + (row + 1) * 100 + "px," + col * 100 + "px)";
+        alert("rect(" + row * 100 + "px," + (col + 1) * 100 + "px," + (row + 1) * 100 + "px," + col * 100 + "px)");
+
+        newNode.innerText = i + "";
 
         puzzleArea.appendChild(newNode);
     }
