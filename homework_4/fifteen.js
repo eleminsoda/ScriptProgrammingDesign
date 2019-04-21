fed073a04a615ff1c2ec685b9a83906a26b16483
lord-maul <987420788yh@gmail.com>
2019-04-21 18:15:32 +0800

    hw4已完成，存一遍版本，以供之后翻新

--- homework_4/fifteen.js
+++ homework_4/fifteen.js
@@ -1,6 +1,10 @@
 'use strict'
 
-window.onload = function () {
+/** row and column indexes start from 0
+ *  array index starts from 1
+ */
+
+window.onload = function() {
     let shuffleButton = document.getElementById('shufflebutton');
     shuffleButton.onclick = shuffle;
 
@@ -9,6 +13,7 @@ window.onload = function () {
 
 let puzzleSize = 4;
 let puzzleNodes = [];
+let nowIndex = [];
 let blankIndex = 16;
 
 function insertPuzzle() {
@@ -21,25 +26,127 @@ function insertPuzzle() {
         let row = Math.floor((i - 1) / puzzleSize);
         let col = (i - 1) % puzzleSize;
         newNode.id = "node_" + row + "_" + col;
-
-        newNode.style.border = "5px solid black";
-        newNode.style.width = "100px";
-        newNode.style.height = "100px";
-        newNode.style.position = "absolute";
         newNode.style.left = col * 100 + "px";
         newNode.style.top = row * 100 + "px";
-        newNode.style.backgroundImage = "url(background.jpg)";
+        newNode.onclick = move;
 
+        // paint every piece of puzzle
+        if (i !== blankIndex) {
+            newNode.classList.add('showingpiece');
 
-        newNode.style.clip = "rect(" + row * 100 + "px," + (col + 1) * 100 + "px," + (row + 1) * 100 + "px," + col * 100 + "px)";
-        alert("rect(" + row * 100 + "px," + (col + 1) * 100 + "px," + (row + 1) * 100 + "px," + col * 100 + "px)");
+            newNode.style.backgroundPosition = col * (-100) + "px " + row * (-100) + "px";
 
-        newNode.innerText = i + "";
+            newNode.innerText = i;
+
+        }
 
         puzzleArea.appendChild(newNode);
+        puzzleNodes.push(newNode);
+        nowIndex.push(i);
     }
 }
 
-function shuffle() {
+// move the tile if it's movable
+function move() {
+    console.log("move called, " + this)
+    let row = parseInt(this.id.charAt(5));
+    let col = parseInt(this.id.charAt(7));
+    let index = row * puzzleSize + col + 1;
 
+    if (!judgeMovable(index)) return;
+
+    exchange(index);
+    render();
+}
+
+// exchange the tile with the empty tile
+function exchange(index, lasy) {
+    // let node = puzzleNodes[index - 1];
+
+    // deactivate the tile
+    // node.classList.remove('showingpiece');
+    // node.innerText = "";
+    // node.onclick = undefined;
+
+    // activate the empty tile
+    let realIndex = nowIndex[index - 1];
+    // let realRow = Math.floor((realIndex - 1) / puzzleSize);
+    // let realCol = (realIndex - 1) % puzzleSize;
+    // let blankNode = puzzleNodes[blankIndex - 1];
+
+    // blankNode.classList.add('showingpiece');
+    // blankNode.style.backgroundPosition = realCol * (-100) + "px " + realRow * (-100) + "px";
+    // blankNode.innerText = realIndex;
+    // blankNode.onclick = move;
+
+    // fresh the indexes
+    // console.log("nowIndex: " + nowIndex);
+    // console.log("blankIndex: " + blankIndex);
+    // console.log("the index detected: " + index)
+    // console.log("real row and col: " + blankNode.backgroundPosition);
+
+    nowIndex[blankIndex - 1] = realIndex;
+    nowIndex[index - 1] = -1;
+    blankIndex = index;
+
+    // console.log("refreshed nowIndex: " + nowIndex);
+    // console.log("--------------------")
+
+}
+
+// judge whether a tile is movable
+function judgeMovable(index) {
+    let neighborArr = findMovableNeighborIndex();
+    if (neighborArr.indexOf(index) === -1) {
+        return false;
+    }
+    return true;
+}
+
+// render the whole puzzle according to the new indexes
+function render() {
+    for (let index = 1; index <= puzzleSize * puzzleSize; index++) {
+        let node = puzzleNodes[index - 1];
+        let realIndex = nowIndex[index - 1];
+
+        if (realIndex !== -1) {
+            let realRow = Math.floor((realIndex - 1) / puzzleSize);
+            let realCol = (realIndex - 1) % puzzleSize;
+
+            node.classList.add('showingpiece');
+            node.style.backgroundPosition = realCol * (-100) + "px " + realRow * (-100) + "px";
+            node.innerText = realIndex;
+            node.onclick = move;
+        } else {
+            node.classList.remove('showingpiece');
+            node.innerText = "";
+            node.onclick = undefined;
+        }
+    }
+}
+
+function findMovableNeighborIndex() {
+    let increment = [(-1) * puzzleSize, -1, 1, puzzleSize];
+    let neighborArr = [];
+
+    let temp = 0;
+    for (let i of increment) {
+        temp = blankIndex + i;
+        if (temp >= 1 && temp <= (puzzleSize * puzzleSize)) {
+            neighborArr.push(temp);
+        }
+    }
+
+    return neighborArr;
+}
+
+function shuffle() {
+    for (let i = 0; i < 1000; i++) {
+        let neighborArr = findMovableNeighborIndex();
+        let length = neighborArr.length;
+        let nextStep = parseInt(Math.random() * length);
+
+        exchange(neighborArr[nextStep]);
+    }
+    render();
 }
