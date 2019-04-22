4e6b6828dc8970929d9d3385358789078f84720a
lord-maul <987420788yh@gmail.com>
2019-04-22 09:33:57 +0800

    add jsdoc

--- homework_3/speedreader.js
+++ homework_3/speedreader.js
@@ -1,164 +1,182 @@
 // The JS file for the homework3, Script Programming design, Spring 2019
+(function() {
+    "use strict";
 
-'use strict'
+    // when the page content finishes loading ...
+    window.onload = function() {
+        console.log("window.onload, start binding...");
+        // set click triggers
+        document.getElementById('start').onclick = startAnimation;
+        document.getElementById('stop').onclick = stopAnimation;
 
-// when the page content finishes loading ...
-window.onload = function() {
-    console.log("window.onload, start binding...");
-    // set click triggers
-    document.getElementById('start').onclick = startAnimation;
-    document.getElementById('stop').onclick = stopAnimation;
+        // set trigger for changes of the font size
+        let sizeNodes = document.getElementsByName("size");
+        for (let node of sizeNodes) {
+            node.onchange = changeFontSize;
+        }
 
-    // set trigger for changes of the font size
-    let sizeNodes = document.getElementsByName("size");
-    for (let node of sizeNodes) {
-        node.onchange = changeFontSize;
-    }
+        // set trigger for changes of the display speed
+        document.getElementById('speed').onchange = changeDisplaySpeed;
+        disableButton('stop');
+    };
 
-    // set trigger for changes of the display speed
-    document.getElementById('speed').onchange = changeDisplaySpeed;
-    disableButton('stop');
-}
+    let displayList = [];
+    let interval = 0;
+    let index = -1;
 
-let displayList = [];
-let interval = 0;
-let index = -1;
+    /**
+     * called the the start button is pressed
+     */
+    function startAnimation() {
+        let showArea = document.getElementById("show-area");
 
-// called the the start button is pressed
-function startAnimation() {
-    let showArea = document.getElementById("show-area");
+        let text = document.getElementById('text-input').value;
+        let wordList = text.trim().split(/\s+/);
 
-    let text = document.getElementById('text-input').value;
-    let wordList = text.trim().split(/\s+/);
-
-    if (wordList.length == 1 && wordList[0] === '') {
-        console.log("the text area is empty");
-        return;
-    } else {
-        setDisableStatus('start');
-    }
-
-    displayList = [];
-    let punctuation = [',', '.', '!', '?', ':', ";", "，", "。", "？", "；", "："];
-    for (let word of wordList) {
-        let lastCh = word.charAt(word.length - 1);
-        let newWord = '';
-        if (punctuation.indexOf(lastCh) !== -1) {
-            newWord = word.slice(0, -1);
-            // push the word twice if it ends with a punctuation mark
-            displayList.push(newWord);
-            displayList.push(newWord);
+        if (wordList.length == 1 && wordList[0] === '') {
+            console.log("the text area is empty");
+            return;
         } else {
-            displayList.push(word);
+            setDisableStatus('start');
+        }
+
+        displayList = [];
+        let punctuation = [',', '.', '!', '?', ':', ";", "，", "。", "？", "；", "："];
+        for (let word of wordList) {
+            let lastCh = word.charAt(word.length - 1);
+            let newWord = '';
+            if (punctuation.indexOf(lastCh) !== -1) {
+                newWord = word.slice(0, -1);
+                // push the word twice if it ends with a punctuation mark
+                displayList.push(newWord);
+                displayList.push(newWord);
+            } else {
+                displayList.push(word);
+            }
+        }
+
+        // because of the feature of clearInterval, 
+        // we have to add the last word twice into the queue
+        let lastWord = displayList[displayList.length - 1];
+        displayList.push(lastWord);
+
+        let speed = parseInt(document.getElementById('speed').value);
+        index = 0;
+        // start displaying the words
+        interval = setInterval(() => {
+            showArea.innerText = displayList[index];
+            console.log("now index: " + index + ", word: " + displayList[index]);
+            index += 1;
+
+            if (index >= displayList.length) {
+                stopAnimation();
+            }
+        }, speed);
+    }
+
+    /**
+     * stop the animation
+     */
+    function stopAnimation() {
+        console.log("stop animation");
+        clearInterval(interval);
+        let showArea = document.getElementById("show-area");
+        showArea.innerText = "";
+        setDisableStatus('stop');
+    }
+
+    /**
+     * change the font size according to the size selected
+     */
+    function changeFontSize() {
+        let sizeNodes = document.getElementsByName("size");
+        for (let node of sizeNodes) {
+            // get the node that is checked
+            if (node.checked === true) {
+                console.log("Already found the fontSize node...");
+
+                let showArea = document.getElementById("show-area");
+                console.log("fontSize changed to " + node.value);
+                showArea.style.fontSize = node.value;
+                break;
+            }
         }
     }
 
-    // because of the feature of clearInterval, we have to add the last word twice into the queue
-    let lastWord = displayList[displayList.length - 1];
-    displayList.push(lastWord);
+    /**
+     * change display speed
+     */
+    function changeDisplaySpeed() {
+        let newSpeed = parseInt(document.getElementById('speed').value);
+        console.log('changing display speed to ' + newSpeed);
 
-    let speed = parseInt(document.getElementById('speed').value);
-    index = 0;
-    // start displaying the words
-    interval = setInterval(() => {
-        showArea.innerText = displayList[index];
-        console.log("now index: " + index + ", word: " + displayList[index])
-        index += 1;
+        // clear the interval first
+        clearInterval(interval);
 
-        if (index >= displayList.length) {
-            stopAnimation();
+        // set new interval based on new display speed
+        let showArea = document.getElementById("show-area");
+
+        // if the display hasn't start yet...
+        if (index < 0) {
+            return;
         }
-    }, speed);
-}
 
-// stop the animation
-function stopAnimation() {
-    console.log("stop animation");
-    clearInterval(interval);
-    let showArea = document.getElementById("show-area");
-    showArea.innerText = "";
-    setDisableStatus('stop');
-}
+        interval = setInterval(() => {
+            showArea.innerText = displayList[index];
+            console.log("now index: " + index + ", word: " + displayList[index]);
+            index += 1;
 
-// change the font size according to the size selected
-function changeFontSize() {
-    let sizeNodes = document.getElementsByName("size");
-    for (let node of sizeNodes) {
-        // get the node that is checked
-        if (node.checked === true) {
-            console.log("Already found the fontSize node...");
+            if (index >= displayList.length) {
+                stopAnimation();
+            }
+        }, newSpeed);
+    }
 
-            let showArea = document.getElementById("show-area");
-            console.log("fontSize changed to " + node.value);
-            showArea.style.fontSize = node.value;
-            break;
+    /**
+     * disable the start or stop button
+     * @param {number} type the type of button
+     */
+    function disableButton(type) {
+        let another = '';
+        if (type === 'start') {
+            another = 'stop';
+        } else if (type === 'stop') {
+            another = 'start';
+        } else {
+            alert("Type error!");
+            return;
+        }
+
+        let button = document.getElementById(type);
+        button.disabled = true;
+        button.style.backgroundColor = 'lightgray';
+
+        button = document.getElementById(another);
+        button.disabled = false;
+        button.style.backgroundColor = 'white';
+    }
+
+    /**
+     * set the disable status of the textarea
+     */
+    function setTextArea() {
+        let textarea = document.getElementById('text-input');
+        let status = textarea.disabled;
+
+        if (status) {
+            textarea.disabled = false;
+        } else {
+            textarea.disabled = true;
         }
     }
-}
 
-// change display speed
-function changeDisplaySpeed() {
-    let newSpeed = parseInt(document.getElementById('speed').value);
-    console.log('changing display speed to ' + newSpeed);
 
-    // clear the interval first
-    clearInterval(interval);
-
-    // set new interval based on new display speed
-    let showArea = document.getElementById("show-area");
-    let wordNow = showArea.innerText;
-
-    // if the display hasn't start yet...
-    if (index < 0) {
-        return;
+    /**
+     * set the disable status for both the button and the textarea
+     * @param {number} type the type of button
+     */
+    function setDisableStatus(type) {
+        disableButton(type);
+        setTextArea();
     }
-
-    interval = setInterval(() => {
-        showArea.innerText = displayList[index];
-        console.log("now index: " + index + ", word: " + displayList[index])
-        index += 1;
-
-        if (index >= displayList.length) {
-            stopAnimation();
-        }
-    }, newSpeed);
-}
-
-// disable the start or stop button
-function disableButton(type) {
-    let another = '';
-    if (type === 'start') {
-        another = 'stop';
-    } else if (type === 'stop') {
-        another = 'start';
-    } else {
-        alert("Type error!");
-        return;
-    }
-
-    let button = document.getElementById(type);
-    button.disabled = true;
-    button.style.backgroundColor = 'lightgray';
-
-    button = document.getElementById(another);
-    button.disabled = false;
-    button.style.backgroundColor = 'white';
-}
-
-// set the disable status of the textarea
-function setTextArea() {
-    let textarea = document.getElementById('text-input');
-    let status = textarea.disabled;
-
-    if (status) {
-        textarea.disabled = false;
-    } else {
-        textarea.disabled = true;
-    }
-}
-
-// set the disable status for both the button and the textarea
-function setDisableStatus(type) {
-    disableButton(type);
-    setTextArea();
-}
+})();
--- homework_4/fifteen.js
+++ homework_4/fifteen.js
@@ -1,127 +1,148 @@
-'use strict';
+(function() {
+    'use strict';
 
-/** row and column indexes start from 0
- *  array index starts from 1
- */
+    /** row and column indexes start from 0
+     *  array index starts from 1
+     */
+    window.onload = function() {
+        let shuffleButton = document.getElementById('shufflebutton');
+        shuffleButton.onclick = shuffle;
 
-window.onload = function() {
-    let shuffleButton = document.getElementById('shufflebutton');
-    shuffleButton.onclick = shuffle;
+        insertPuzzle();
+    };
 
-    insertPuzzle();
-}
+    let puzzleSize = 4;
+    let puzzleNodes = [];
+    let nowIndex = [];
+    let blankIndex = 16;
 
-let puzzleSize = 4;
-let puzzleNodes = [];
-let nowIndex = [];
-let blankIndex = 16;
+    /**
+     * initialize the whole puzzle
+     */
+    function insertPuzzle() {
+        let puzzleArea = document.getElementById('puzzlearea');
+        let puzzleCount = puzzleSize * puzzleSize;
 
-// initialize the whole puzzle
-function insertPuzzle() {
-    let puzzleArea = document.getElementById('puzzlearea');
-    let puzzleCount = puzzleSize * puzzleSize;
+        // create each node in for loop
+        for (let i = 1; i <= puzzleCount; i++) {
+            let newNode = document.createElement('div');
+            let row = Math.floor((i - 1) / puzzleSize);
+            let col = (i - 1) % puzzleSize;
+            newNode.id = "node_" + row + "_" + col;
+            newNode.style.left = col * 100 + "px";
+            newNode.style.top = row * 100 + "px";
+            newNode.onclick = move;
 
-    // create each node in for loop
-    for (let i = 1; i <= puzzleCount; i++) {
-        let newNode = document.createElement('div');
-        let row = Math.floor((i - 1) / puzzleSize);
-        let col = (i - 1) % puzzleSize;
-        newNode.id = "node_" + row + "_" + col;
-        newNode.style.left = col * 100 + "px";
-        newNode.style.top = row * 100 + "px";
-        newNode.onclick = move;
+            // paint every piece of puzzle
+            if (i !== blankIndex) {
+                newNode.classList.add('showingpiece');
+                newNode.style.backgroundPosition = col * (-100) + "px " + row * (-100) + "px";
+                newNode.innerText = i;
+            }
 
-        // paint every piece of puzzle
-        if (i !== blankIndex) {
-            newNode.classList.add('showingpiece');
-            newNode.style.backgroundPosition = col * (-100) + "px " + row * (-100) + "px";
-            newNode.innerText = i;
+            puzzleArea.appendChild(newNode);
+            puzzleNodes.push(newNode);
+            nowIndex.push(i);
+        }
+    }
+
+    /**
+     * move the tile if it's movable
+     */
+    function move() {
+        console.log("move called, " + this);
+        let row = parseInt(this.id.charAt(5));
+        let col = parseInt(this.id.charAt(7));
+        let index = row * puzzleSize + col + 1;
+
+        if (!judgeMovable(index)) {
+            return;
         }
 
-        puzzleArea.appendChild(newNode);
-        puzzleNodes.push(newNode);
-        nowIndex.push(i);
+        exchange(index);
+        render();
     }
-}
 
-// move the tile if it's movable
-function move() {
-    console.log("move called, " + this)
-    let row = parseInt(this.id.charAt(5));
-    let col = parseInt(this.id.charAt(7));
-    let index = row * puzzleSize + col + 1;
-
-    if (!judgeMovable(index)) return;
-
-    exchange(index);
-    render();
-}
-
-// exchange the tile with the empty tile. 
-// as it only deal with indexes, it's very fast
-function exchange(index, lasy) {
-    let realIndex = nowIndex[index - 1];
-
-    nowIndex[blankIndex - 1] = realIndex;
-    nowIndex[index - 1] = -1;
-    blankIndex = index;
-}
-
-// judge whether a tile is movable
-function judgeMovable(index) {
-    let neighborArr = findMovableNeighborIndex();
-    if (neighborArr.indexOf(index) === -1) {
-        return false;
-    }
-    return true;
-}
-
-// render the whole puzzle according to the new indexes
-function render() {
-    for (let index = 1; index <= puzzleSize * puzzleSize; index++) {
-        let node = puzzleNodes[index - 1];
+    /**
+     * exchange the tile with the empty tile. 
+     * as it only deal with indexes, it's very fast
+     * @param {number} index the index of the tile you want to exchange
+     */
+    function exchange(index) {
         let realIndex = nowIndex[index - 1];
 
-        if (realIndex !== -1) {
-            let realRow = Math.floor((realIndex - 1) / puzzleSize);
-            let realCol = (realIndex - 1) % puzzleSize;
-
-            node.classList.add('showingpiece');
-            node.style.backgroundPosition = realCol * (-100) + "px " + realRow * (-100) + "px";
-            node.innerText = realIndex;
-            node.onclick = move;
-        } else {
-            node.classList.remove('showingpiece');
-            node.innerText = "";
-            node.onclick = undefined;
-        }
-    }
-}
-
-// get all the indexes of the neighbors that are movable
-function findMovableNeighborIndex() {
-    let increment = [(-1) * puzzleSize, -1, 1, puzzleSize];
-    let neighborArr = [];
-
-    let temp = 0;
-    for (let i of increment) {
-        temp = blankIndex + i;
-        if (temp >= 1 && temp <= (puzzleSize * puzzleSize)) {
-            neighborArr.push(temp);
-        }
+        nowIndex[blankIndex - 1] = realIndex;
+        nowIndex[index - 1] = -1;
+        blankIndex = index;
     }
 
-    return neighborArr;
-}
-
-// shuffle the whole puzzle, adopt lasy rendering technique
-function shuffle() {
-    for (let i = 0; i < 1000; i++) {
+    /**
+     * judge whether a tile is movable
+     * @param {number} index the index of tile you want to judge
+     * @returns {boolean} whether the tile is movable
+     */
+    function judgeMovable(index) {
         let neighborArr = findMovableNeighborIndex();
-        let length = neighborArr.length;
-        let nextStep = parseInt(Math.random() * length);
-
-        exchange(neighborArr[nextStep]);
+        if (neighborArr.indexOf(index) === -1) {
+            return false;
+        }
+        return true;
     }
-    render();
-}
+
+    /**
+     * render the whole puzzle according to the new indexes
+     */
+    function render() {
+        for (let index = 1; index <= puzzleSize * puzzleSize; index++) {
+            let node = puzzleNodes[index - 1];
+            let realIndex = nowIndex[index - 1];
+
+            if (realIndex !== -1) {
+                let realRow = Math.floor((realIndex - 1) / puzzleSize);
+                let realCol = (realIndex - 1) % puzzleSize;
+
+                node.classList.add('showingpiece');
+                node.style.backgroundPosition = realCol * (-100) + "px " + realRow * (-100) + "px";
+                node.innerText = realIndex;
+                node.onclick = move;
+            } else {
+                node.classList.remove('showingpiece');
+                node.innerText = "";
+                node.onclick = undefined;
+            }
+        }
+    }
+
+    /**
+     * get all the indexes of the neighbors that are movable
+     * @returns {boolean} the neighbor array
+     */
+    function findMovableNeighborIndex() {
+        let increment = [(-1) * puzzleSize, -1, 1, puzzleSize];
+        let neighborArr = [];
+
+        let temp = 0;
+        for (let i of increment) {
+            temp = blankIndex + i;
+            if (temp >= 1 && temp <= (puzzleSize * puzzleSize)) {
+                neighborArr.push(temp);
+            }
+        }
+
+        return neighborArr;
+    }
+
+    /**
+     * shuffle the whole puzzle, adopt lasy rendering technique
+     */
+    function shuffle() {
+        for (let i = 0; i < 1000; i++) {
+            let neighborArr = findMovableNeighborIndex();
+            let length = neighborArr.length;
+            let nextStep = parseInt(Math.random() * length);
+
+            exchange(neighborArr[nextStep]);
+        }
+        render();
+    }
+})();
